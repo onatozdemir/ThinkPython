3ff3683c094e2f7faf9e6a277ebe9ed728fcd346
umonoz <onatozdemir@gmail.com>
2020-09-13 12:10:24 +0300

    more than 20

---
+++ 9-1.py
@@ -0,0 +1,14 @@
+## Solution for Think Python Chapter 9, Exercise 1
+## Words with more than 20 characters
+
+fin = open('words.txt')
+line = fin.readline()
+
+
+
+while True:
+    word = line.strip()
+    if len(word) > 20:
+        print(word)
+
+
