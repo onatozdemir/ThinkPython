7aa89d61741c557d1cfa7c076032a5f6ff72408c
umonoz <onatozdemir@gmail.com>
2020-09-14 01:02:23 +0300

    9-3

--- 9-3.py
+++ 9-3.py
@@ -1,12 +1,15 @@
-##Solution to Think Python, Chapter 9, Exercise 3
+##Solution to Think Python, Chapter 9, Exercise 3, Part 1
 ##Forbidden Letters
 
 def avoids():
-    c = str(input('please enter a letter: '))
+    c = str(input('please enter letters: '))
     b = str(input('please enter a word: '))
     for a in b:
-        if a == c:
-            return False
+        num = 0
+        while num < len(c):
+            if a == c[num]:
+                return False
+            num += 1
     return True
 
 print(avoids())
